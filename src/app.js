deab27252b42eea8402d36cca8278d2c4aaf1a39
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-24 15:16:16 +0000

    [Generador de Cartas Aleatorio] app.js- se estabecen las funciones, la condicion para cuando es rojo y negro,  botón y temporizador de intervalo para generación automática. index.html-carta y componentes mediante divs, botón. style.css- color de fondo, posición de elementos y tamaño de fuente.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 window.onload = function() {
-  //write your code here
+  const paloCarta = ["♦", "♥", "♠", "♣"];
+  const numeroCarta = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function seleccionarAleatorio(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function generarCarta() {
+    const randomPalo = seleccionarAleatorio(paloCarta);
+    const randomNumero = seleccionarAleatorio(numeroCarta);
+    let cartaAleatoria = "";
+    let colorPalo = randomPalo === "♥" || randomPalo === "♦" ? "red" : "";
+
+    cartaAleatoria += `
+      <div class="paloArriba" style="color: ${colorPalo};">${randomPalo}</div>
+      <div class="numeroCarta">${randomNumero}</div>
+      <div class="paloAbajo" style="color: ${colorPalo};">${randomPalo}</div>
+    `;
+
+    document.querySelector(".card-body").innerHTML = cartaAleatoria;
+    const generarButton = document.getElementById("generar");
+    generarButton.addEventListener("click", generarCarta);
+  }
+  generarCarta();
+  setInterval(generarCarta, 10000);
 };
